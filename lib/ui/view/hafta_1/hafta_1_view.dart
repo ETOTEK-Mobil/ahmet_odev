bd419d5f88285bbfad8f01146fd5bd867cc6a6bc
Berke Enes Aktümen <81863556+berkeenesakt@users.noreply.github.com>
2024-11-30 12:22:58 +0300

    Refactor and improve code readability by renaming classes and variables, updating widget properties, and ensuring consistent formatting across multiple files.

--- lib/ui/view/hafta_1/hafta_1_view.dart
+++ lib/ui/view/hafta_1/hafta_1_view.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ahmet_odev/ui/view/pages/column_raw_stack_page.dart';
-import 'package:ahmet_odev/ui/view/pages/stage_management_page.dart';
 
 import 'pages/column_raw_stack_page.dart';
 import 'pages/stage_management_page.dart';
@@ -8,6 +6,7 @@ import 'pages/stage_management_page.dart';
 void main() {
   runApp(const Hafta1View());
 }
+
 class Hafta1View extends StatelessWidget {
   const Hafta1View({super.key});
 
@@ -23,6 +22,7 @@ class Hafta1View extends StatelessWidget {
     );
   }
 }
+
 //!Burası ne işe yarıyor bulamadım, yardıma açığım.
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -31,22 +31,24 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
 //! AppSize Boyutlarımız.
 class AppSize {
-  static const double sm = 8.0;  // Small spacing
+  static const double sm = 8.0; // Small spacing
   static const double md = 16.0; // Medium spacing
   static const double lg = 32.0; // Large spacing
 }
+
 //! Ana Sayfa
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        Centertitle(true),
-        title: Center(
+        centerTitle: true,
+        title: const Center(
           child: Text("1.Hafta"),
-        )
+        ),
       ),
       body: Center(
         child: Column(
@@ -54,19 +56,19 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             //? Button 1
             ElevatedButton(
-              child: Text("Row-Column-Stack Kullanımı"),
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => col_row_stack_page()));
+              child: const Text("Row-Column-Stack Kullanımı"),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const ColumnRowStackPage()));
               },
             ),
             //? İki buton arasına koyduğumuz boşluk
             const SizedBox(height: AppSize.md),
             //? Button 2
             ElevatedButton(
-            child: Text("State Management"),
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => stage_mana_page()));
-            },
+              child: const Text("State Management"),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const StageManagementPage()));
+              },
             )
           ],
         ),
